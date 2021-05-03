e1fe125a46235f52870cc07a60a9d06598e09d7f
Nguyễn Minh Đức <mducbg2000@gmail.com>
2021-05-03 08:42:09 +0000

    Revert "Merge branch 'dhd' into 'master'"
    This reverts merge request !15

--- public/js/group_catch_events.js
+++
@@ -1,4 +0,0 @@
-groupSearchingInput.addEventListener("search", handleGroupSearching);
-modalRemovingIcon.addEventListener("click", handleModalRemoval);
-postCreatorArea.addEventListener("click", openPostCreatorModal);
-textArea.addEventListener("keyup", handleTextAreaValue);
