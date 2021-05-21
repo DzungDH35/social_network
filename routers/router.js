11a5df10c22cd6de9f98bc3138b2e64a7aea5e18
DzungDH35 <dzung.dh3510@gmail.com>
2021-05-21 22:52:20 +0700

    New UI: framechat

--- routers/router.js
+++ routers/router.js
@@ -11,6 +11,7 @@ router.use('/group', require('./group'));
 router.use('/search_result', require('./search_result'));
 router.use('/create_group', require('./create_group'))
 router.use('/comment', require('./comment'))
+router.use('/framechat', require('./framechat'))
 
 
 module.exports = router;
